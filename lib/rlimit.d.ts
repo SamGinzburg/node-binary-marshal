0f2fa78f5812e418d163cca3fba175d7932ea0ed
SamGinzburg <ginzburg.sam@gmail.com>
2018-04-11 20:09:35 -0400

    updated output files

--- lib/rlimit.d.ts
+++ lib/rlimit.d.ts
@@ -1,6 +1,8 @@
 import { StructDef } from './marshal';
 export declare const rlimit: StructDef;
 export interface RlimitIn {
+    padding1: number;
     rlim_cur: number;
     rlim_max: number;
+    padding2: number;
 }
