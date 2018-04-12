5fb415b5b4362c8751b732ec704e20663bea62a0
SamGinzburg <ginzburg.sam@gmail.com>
2018-04-11 20:00:48 -0400

    added padding for rlimit struct

--- src/rlimit.ts
+++ src/rlimit.ts
@@ -2,14 +2,18 @@ import { StructDef, Marshal, Unmarshal, isZero } from './marshal';
 
 export const rlimit: StructDef = {
 	fields: [
-		{name: 'rlim_cur',    type: 'uint64'},
-		{name: 'rlim_max',    type: 'uint64'},
+		{name: 'padding1',    type: 'uint32'},
+		{name: 'rlim_cur',    type: 'uint32'},
+		{name: 'padding2',    type: 'uint32'},
+		{name: 'rlim_max',    type: 'uint32'},
 	],
 	alignment: 'natural', // 'packed'
 	length: 128,
 };
 
 export interface RlimitIn {
+	padding1:   number;
 	rlim_cur:   number;
 	rlim_max:   number;
+	padding2:   number;
 };
